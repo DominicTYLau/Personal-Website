33156e56a2d674ff4217694f47a0ae1e7723793c
DominicTYLau <dlau1480@gmail.com>
2024-11-05 20:44:57 -0500

    dist

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(() => {
   return {
     build: {
-      outDir: 'build',
+      outDir: 'dist', // Change this to 'dist' for default output directory
     },
     plugins: [react()],
     assetsInclude: ['**/*.glb']

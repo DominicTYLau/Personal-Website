57d187b7ef7ea7e28dd9a678f9f2d594bdb4f16e
DominicTYLau <dlau1480@gmail.com>
2024-11-14 20:14:51 -0500

    update links

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -58,8 +58,6 @@ export default class Home extends Component {
 							<svg
 								viewBox="0 0 24 24"
 								fill="currentColor"
-								height="3rem"
-								width="3rem"
 							>
 								<path d="M11.178 19.569a.998.998 0 001.644 0l9-13A.999.999 0 0021 5H3a1.002 1.002 0 00-.822 1.569l9 13z" />
 							</svg>
